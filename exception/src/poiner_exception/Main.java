8f48ee6915ece611ee6344d8531c6a48dd8165de
Maksim Bazyk <musik.bazyk@gmail.com>
2020-03-02 22:34:26 +0200

    added method for checks Exception

---
+++ exception/src/poiner_exception/Main.java
@@ -0,0 +1,9 @@
+package poiner_exception;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Methods methods = new Methods();
+        methods.calculateVariable();
+    }
+}
---
+++ exception/src/poiner_exception/MyException.java
@@ -0,0 +1,8 @@
+package poiner_exception;
+
+public class MyException extends Exception {
+
+    public MyException(String message) {
+        super(message);
+    }
+}
